4e8166136ba5e9d9bdc90e1349b344bf27c56207
Patricio-Espinoza-Rojas <p.alfonsoespinoza@gmail.com>
2022-07-06 19:23:42 +0000

    prueba

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,64 +1,22 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
+/* import { Footer } from "./Footer";
+import { CardContainer } from "./cardgroup"; */
 
 //create your first component
-const Home = () => {
-  return (
-    <>
-      <Navbar title="Start Boostra" />
-      <Jumbotron
-        tilte="A Warm Welcome"
-        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-		  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
-		  eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        button="Call to action!"
-      />
-
-      <div className="row m-3">
-        <card
-          img=""
-          tilte="Card tile"
-          button="find out more!"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-		  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
-		  eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        />
-        <card
-          img="http://placekitten.com/500/325"
-          tilte="Card tile"
-          button="find out more!"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim 
-		  ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse 
-		  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        />
-        <card
-          img="http://placekitten.com/500/325"
-          tilte="Card tile"
-          button="find out more!"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-		  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-		  fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        />
-        <card
-          img="http://placekitten.com/500/325"
-          tilte="Card tile"
-          button="find out more!"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, 
-		  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
-		  fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        />
-      </div>
-      <footer>
-        <div class="row">
-          <div class="col-lg-12">
-            <p>Copyright &copy; Your Website Name 2021</p>
-          </div>
-        </div>
-      </footer>
-    </>
-  );
-};
-
-export default Home;
+export function Home() {
+	return (
+		<div>
+			<Navbar />
+			<Jumbotron
+				title="A Warm Welcome!"
+				description="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here'"
+				buttonLabel="Click here for more details"
+				buttonURL="https://reactjs.org/"
+			/>
+		{/* 	<CardContainer />
+			<Footer /> */}
+		</div>
+	);
+}
