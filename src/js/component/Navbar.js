fbe74bda7bc5ef939ead468e3cab13bbc0d398b1
Deby92 <d.cantuarias.urrutia@gmail.com>
2022-07-16 01:31:26 +0000

    viernes

--- src/js/component/Navbar.js
+++ src/js/component/Navbar.js
@@ -3,25 +3,25 @@ import React from "react";
 export const Navbar = () => {
   return (
     <div className="container-fluid navbar navbar lg bg-dark">
-      <a className="navbar-brand">Start React & Bootstrap</a>
+      <a className="navbar-brand text-light">Start React & Bootstrap</a>
       <ul className="nav justify-content-end">
         <li className="nav-item">
-          <a className="nav-link" aria-current="page" href="#">
+          <a className="nav-link text-secondary" aria-current="page" href="#">
             Home
           </a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">
+        <li className="nav-item ">
+          <a className="nav-link text-secondary" href="#">
             About
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">
+          <a className="nav-link text-secondary" href="#">
             Services
           </a>
         </li>
         <li className="nav-item">
-          <a className="nav-link">Contact</a>
+          <a className="nav-link text-secondary">Contact</a>
         </li>
       </ul>
     </div>
